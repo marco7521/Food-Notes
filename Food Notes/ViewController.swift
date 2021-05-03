f3b51e27f1b48a657471a1b714a9b6d8e07aff0b
Marco <marco7521@gamil.com>
2021-05-03 15:21:06 +0800

    Initial Commit

---
+++ Food Notes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Food Notes
+//
+//  Created by admin on 2021/5/3.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
